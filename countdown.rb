4abb4356652971c6a7f29fca7ddfd29e09ded43b
Crystal Norman <cnorm88@gmail.com>
2020-09-11 19:16:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,14 @@
 #write your code here
-def countdown
+def countdown(number)
+  
 end
+
+
+# def countdown(number)
+#   while number > 10
+#   puts "#{number} SECOND(S)!"
+#   number -= 1
+#   end
+#   end
+
+# puts "HAPPY NEW YEAR!"
